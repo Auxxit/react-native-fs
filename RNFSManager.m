002ba9ad676b09f667a65d21474f3270df4e3241
David Vorona <davidavorona@gmail.com>
2022-01-17 19:42:33 -0800

    Implement fix for iOS nil value issue

--- RNFSManager.m
+++ RNFSManager.m
@@ -58,25 +58,27 @@ RCT_EXPORT_METHOD(readDir:(NSString *)dirPath
 
   NSArray *contents = [fileManager contentsOfDirectoryAtPath:dirPath error:&error];
 
-  contents = [contents rnfs_mapObjectsUsingBlock:^id(NSString *obj, NSUInteger idx) {
+ NSMutableArray *targetContents = [[NSMutableArray alloc] init];
+  for (NSString *obj in contents) {
     NSString *path = [dirPath stringByAppendingPathComponent:obj];
     NSDictionary *attributes = [fileManager attributesOfItemAtPath:path error:nil];
-
-    return @{
-             @"ctime": [self dateToTimeIntervalNumber:(NSDate *)[attributes objectForKey:NSFileCreationDate]],
-             @"mtime": [self dateToTimeIntervalNumber:(NSDate *)[attributes objectForKey:NSFileModificationDate]],
-             @"name": obj,
-             @"path": path,
-             @"size": [attributes objectForKey:NSFileSize],
-             @"type": [attributes objectForKey:NSFileType]
-             };
-  }];
+    if(attributes != nil) {
+        [targetContents addObject:@{
+            @"ctime": [self dateToTimeIntervalNumber:(NSDate *)[attributes objectForKey:NSFileCreationDate]],
+            @"mtime": [self dateToTimeIntervalNumber:(NSDate *)[attributes objectForKey:NSFileModificationDate]],
+            @"name": obj,
+            @"path": path,
+            @"size": [attributes objectForKey:NSFileSize],
+            @"type": [attributes objectForKey:NSFileType]
+            }];
+    }
+  }
 
   if (error) {
     return [self reject:reject withError:error];
   }
 
-  resolve(contents);
+  resolve(targetContents);
 }
 
 RCT_EXPORT_METHOD(exists:(NSString *)filepath
